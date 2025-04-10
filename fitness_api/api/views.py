dd2b4288dc20c14773519f7e427f70b879a96f13
takyEhab <tkyehab@gmail.com>
2025-04-10 14:36:18 +0200

    create the view for retrive and update and the urls for it

--- fitness_api/api/views.py
+++ fitness_api/api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Activity
+from .serializers import ActivitySerializer
 
-# Create your views here.
+class ActivityListCreateView(generics.ListCreateAPIView):
+    serializer_class = ActivitySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Activity.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class ActivityRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ActivitySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Activity.objects.filter(user=self.request.user)
