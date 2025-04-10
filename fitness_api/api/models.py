4f0653d18298c621ad494059beccee7e5d83b3ac
takyEhab <tkyehab@gmail.com>
2025-04-10 14:22:03 +0200

    create activity model

--- fitness_api/api/models.py
+++ fitness_api/api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Activity(models.Model):
+    ACTIVITY_CHOICES = [
+        ('Running', 'Running'),
+        ('Cycling', 'Cycling'),
+        ('Weightlifting', 'Weightlifting'),
+        ('Swimming', 'Swimming'),
+        ('Walking', 'Walking'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='activities')
+    activity_type = models.CharField(max_length=50, choices=ACTIVITY_CHOICES)
+    duration = models.PositiveIntegerField(help_text="Duration in minutes")
+    distance = models.FloatField(null=True, blank=True, help_text="Distance in km or miles")
+    calories_burned = models.FloatField(null=True, blank=True)
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.activity_type} - {self.user.username}"
