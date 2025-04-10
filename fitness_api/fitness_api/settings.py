350ce87b89218a0f8b6f89afab9664f84410fb3a
takyEhab <tkyehab@gmail.com>
2025-04-10 14:47:35 +0200

    CREATE TOKEN URL

--- fitness_api/fitness_api/settings.py
+++ fitness_api/fitness_api/settings.py
@@ -39,7 +39,19 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',  
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',  
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
